d9db77b6ecb54450ea61f97929caa52cb3c55bfe
jardmclan <mcleanj@hawaii.edu>
2025-02-24 10:20:26 -1000

    test next config changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,14 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = "";
 let basePath = "/";
 
-if (isGithubActions) {
+if(isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
 }
 
-module.exports = {
+const nextConfig = {
   assetPrefix: assetPrefix,
   basePath: basePath
 };
